6deb657cd09c28532311680d2c52dac9175ef303
ivan.smuniov <smunew.rfkt@gmail.com>
2022-06-27 13:45:04 +0400

    change REST -> event driven design
    added RabbitMQ

--- gamification/src/main/java/microservices/book/gamification/game/GameService.java
+++ gamification/src/main/java/microservices/book/gamification/game/GameService.java
@@ -1,7 +1,7 @@
 package microservices.book.gamification.game;
 
 import lombok.Value;
-import microservices.book.gamification.challenge.ChallengeSolvedDTO;
+import microservices.book.gamification.challenge.ChallengeSolvedEvent;
 import microservices.book.gamification.game.domain.BadgeType;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public interface GameService {
      * @return a {@link GameResult} object containing the new score and badge
      * cards obtained
      */
-    GameResult newAttemptForUser(ChallengeSolvedDTO challenge);
+    GameResult newAttemptForUser(ChallengeSolvedEvent challenge);
 
     @Value
     class GameResult {
--- gamification/src/main/java/microservices/book/gamification/game/badgeprocessors/LuckyNumberBadgeProcessor.java
+++ gamification/src/main/java/microservices/book/gamification/game/badgeprocessors/LuckyNumberBadgeProcessor.java
@@ -1,6 +1,6 @@
 package microservices.book.gamification.game.badgeprocessors;
 
-import microservices.book.gamification.challenge.ChallengeSolvedDTO;
+import microservices.book.gamification.challenge.ChallengeSolvedEvent;
 import microservices.book.gamification.game.domain.BadgeType;
 import microservices.book.gamification.game.domain.ScoreCard;
 import org.springframework.stereotype.Component;
@@ -13,7 +13,7 @@ class LuckyNumberBadgeProcessor implements BadgeProcessor {
     public Optional<BadgeType> processForOptionalBadge(
             int currentScore,
             List<ScoreCard> scoreCardList,
-            ChallengeSolvedDTO solved) {
+            ChallengeSolvedEvent solved) {
             return scoreCardList.size() == 42
                     || solved.getFactorB() == 42 || solved.getFactorA() == 42?
                 Optional.of(BadgeType.LUCKY_NUMBER) : Optional.empty();
