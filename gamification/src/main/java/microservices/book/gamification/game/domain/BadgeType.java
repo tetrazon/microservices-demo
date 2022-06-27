5213f992cb894ccf3cc8f7f5d823e948be03ffd5
ivan.smuniov <smunew.rfkt@gmail.com>
2022-06-27 11:31:08 +0400

    refactor package

---
+++ gamification/src/main/java/microservices/book/gamification/game/domain/BadgeType.java
@@ -0,0 +1,24 @@
+package microservices.book.gamification.game.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Enumeration with the different types of Badges that a user can win.
+ */
+
+@RequiredArgsConstructor
+@Getter
+public enum BadgeType {
+    // Bages depending on score
+    BRONZE("Bronze"),
+    SILVER("Silver"),
+    GOLD("Gold"),
+
+    // Other badges won for different conditions
+    FIRST_WON("First time"),
+    LUCKY_NUMBER("Lucky number");
+
+    private final String description;
+}
